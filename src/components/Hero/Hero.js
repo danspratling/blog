71221f96d8b6180febeb22597305f5d917affaa3
Dan Spratling <danspratling@hotmail.co.uk>
2019-10-09 21:47:37 +0100

    Animated svg heading

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,58 +1,43 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { Container, Row, Col } from '../'
-import theme from '../../gatsby-plugin-theme-ui'
+import { useState, useEffect } from 'react'
+import { Container, Row, Col } from '..'
+import { HeroTitleSVG } from '.'
 
 const Hero = ({ children }) => {
-  const { colors } = theme
+  const [pageLoaded, setPageLoaded] = useState(false)
+
+  useEffect(() => setPageLoaded(true), [])
+
+  const heroStyles = {
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    pt: '160px',
+    px: 4,
+    pb: 4,
+    height: '60vw',
+    maxHeight: '420px',
+  }
+
+  const pathStyles = {
+    path: {
+      strokeDasharray: 300,
+      strokeDashoffset: pageLoaded ? 0 : 300,
+      transition: '1.8s ease',
+    },
+  }
+
   return (
-    <div
-      sx={{
-        position: 'relative',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        pt: '160px',
-        px: 4,
-        pb: 4,
-        height: '60vw',
-        maxHeight: '420px',
-      }}
-    >
-      {/*
-        TODO: Animate the border of this box when the user first sees it
-      */}
-      <div
-        sx={{
-          padding: 4,
-          borderWidth: 6,
-          borderStyle: 'solid',
-          borderColor: 'secondary',
-          backgroundColor: 'background',
-        }}
-      >
-        <Container>
-          <Row>
-            <Col>
-              <h1 sx={{ fontSize: 7, textAlign: 'center', mt: 3, mb: 3 }}>
-                {children}
-              </h1>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <div
-        sx={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          height: `66.66%`,
-          backgroundColor: 'primary',
-          backgroundImage: `linear-gradient(to top, ${colors.primary}, ${colors.primaryGradient})`,
-          zIndex: -1,
-        }}
-      />
+    <div sx={heroStyles}>
+      <Container>
+        <Row>
+          <Col>
+            <HeroTitleSVG sx={pathStyles} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,1 +1,2 @@
-export { default } from "./Hero"
+export { default } from './Hero'
+export { HeroTitleSVG } from './HeroTitleSVG'
