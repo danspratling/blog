8402f52902152525e8d61faaf6a4fde2ad0ecc27
Dan Spratling <danspratling@hotmail.co.uk>
2019-12-27 17:01:46 +0000

    Adds custom favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,7 +72,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `content/assets/gatsby-icon.png`,
+        icon: `src/images/icon.png`,
       },
     },
     `gatsby-plugin-netlify-cms`,
